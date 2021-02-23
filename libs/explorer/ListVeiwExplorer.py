15144b29d89fa671dfe9d7ab4a35ca0c0e6a0471
mohammad javad <mohammadjavad.mirshekari@gmail.com>
2021-02-23 14:02:03 +0330

    Implement 'ListViewExplorer'

---
+++ libs/explorer/ListVeiwExplorer.py
@@ -0,0 +1,38 @@
+from PyQt5.QtWidgets import QFileSystemModel, QDockWidget
+from PyQt5.QtWidgets import QListView
+from PyQt5.QtCore import QModelIndex
+
+
+
+
+class ListView(QListView):
+    def __init__(
+        self,
+        parent=None,
+        onClicked=lambda *argv: None,
+        onDoubleClicked=lambda *argv: None,
+        model=QFileSystemModel(),
+    ):
+        super().__init__()
+        self.parent = parent
+        self.setModel(model)
+        self.clicked.connect(lambda index:
+                             onClicked(*self._translateIndex(index)))
+        self.doubleClicked.connect(lambda index:
+                                   onDoubleClicked(*self._translateIndex(index)))
+        self._configStyle()
+
+    def _configStyle(self):
+        self.setFlow(QListView.LeftToRight)
+        self.setWrapping(True)
+        
+
+    def _translateIndex(self, index):
+        indexItem = self.model().index(index.row(), 0, index.parent())
+        fileName = self.model().fileName(indexItem)
+        filePath = self.model().filePath(indexItem)
+        return fileName, filePath
+
+    def setRootPath(self, path=''):
+        pathIndex = self.model().setRootPath(path)
+        self.setRootIndex(pathIndex)
