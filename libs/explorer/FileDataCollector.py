7bcc3ec2d0bc667594c22eae99059d868b45d28b
mohammad javad <mohammadjavad.mirshekari@gmail.com>
2021-02-25 13:10:50 +0330

    Refactor 'FileDataCollectors' using 'FileTypeCheckingMixin'

--- libs/explorer/FileDataCollector.py
+++ libs/explorer/FileDataCollector.py
@@ -1,40 +1,27 @@
 from ..SingletonMeta import SingletonMeta
+from .Mixins import PngJpegTypeCheckingMixin, DICOMTypeCheckingMixin
 
 
 class AbstractFileDataCollector(metaclass=SingletonMeta):
     def collect(self, path, name):
-        raise Exception("!!! tihs methoud is not implemented !!!")
-
-    def isMyType(self, name):
-        raise Exception("!!! tihs methoud is not implemented !!!")
+        raise Exception("!!! this methoud is not implemented !!!")
 
 
-class PngJpegDataCollector(AbstractFileDataCollector):
+class PngJpegDataCollector(AbstractFileDataCollector, PngJpegTypeCheckingMixin):
     def __init__(self):
-        self.validExt = ['png', 'jpeg']
+        PngJpegTypeCheckingMixin.__init__(self)
 
     def collect(self, path, name):
         extra = {}
         extra['ext'] = name.split('.')[-1]
         return extra
 
-    def isMyType(self, name: str):
-        if name.endswith(tuple(self.validExt)):
-            return True
-        return False
 
-
-class DICOMDataCollector(AbstractFileDataCollector):
+class DICOMDataCollector(AbstractFileDataCollector, DICOMTypeCheckingMixin):
     def __init__(self):
-        self.validExt = ['dcm']
-        self.validPrefix = ['I']
+        DICOMTypeCheckingMixin.__init__(self)
 
     def collect(self, path, name):
         extra = {}
         extra['ext'] = 'dcm'
         return extra
-
-    def isMyType(self, name: str):
-        if name.endswith(tuple(self.validExt)) or name.startswith(tuple(self.validPrefix)):
-            return True
-        return False
