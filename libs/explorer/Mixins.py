859ef0fc5bf76e978af6f8daa95b7a89ea00f89f
mohammad javad <mohammadjavad.mirshekari@gmail.com>
2021-02-25 13:09:41 +0330

    Implement 'FileTypeCheckingMixin' for png, jpeg and DICOM

--- libs/explorer/Mixins.py
+++ libs/explorer/Mixins.py
@@ -1,5 +1,6 @@
 from typing import List, Callable
 
+
 class AbstractExplorerViewMixin:
 
     def _configStyle(self):
@@ -13,17 +14,15 @@ class AbstractExplorerViewMixin:
         """
         raise Exception("!!! this methud not implemented !!!")
 
-
     @property
     def viewModel(self):
         """ return model of view
         """
         raise Exception("!!! this methud not implemented !!!")
-    
 
 
 class AbstractExplorerModelMixin:
-    
+
     @property
     def dataItemsList(self) -> List:
         """ return list of data items
@@ -40,3 +39,32 @@ class AbstractExplorerModelMixin:
         """ scan directory for files and folders
         """
         raise Exception("!!! this methud not implemented !!!")
+
+
+class AbstractFileTypecheckingMixin:
+
+    def isMyType(self, name: str):
+        raise Exception("this methoud is not implemented")
+
+
+class PngJpegTypeCheckingMixin(AbstractFileTypecheckingMixin):
+
+    def __init__(self):
+        self.validExt = ['png', 'jpeg', 'jpg']
+
+    def isMyType(self, name: str):
+        if name.endswith(tuple(self.validExt)):
+            return True
+        return False
+
+
+class DICOMTypeCheckingMixin(AbstractFileTypecheckingMixin):
+
+    def __init__(self):
+        self.validExt = ['dcm']
+        self.validPrefix = ['I']
+
+    def isMyType(self, name: str):
+        if name.endswith(tuple(self.validExt)) or name.startswith(tuple(self.validPrefix)):
+            return True
+        return False
