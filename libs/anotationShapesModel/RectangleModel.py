f52dbd4cf139956b713057f23154b3987021bb42
mohammad javad <mohammadjavad.mirshekari@gmail.com>
2021-03-01 12:47:06 +0330

    Save all points for rectangle instead of two

--- libs/anotationShapesModel/RectangleModel.py
+++ libs/anotationShapesModel/RectangleModel.py
@@ -19,10 +19,14 @@ class RectangleModel(AbstractAnotationShapeModel):
         """ Convert shape information to python dictionary
         """
         return {
-            'x1': self.points[0].x(),
-            'y1': self.points[0].y(),
-            'x2': self.points[1].x(),
-            'y2': self.points[1].y()
+            'x0': self.points[0].x(),
+            'y0': self.points[0].y(),
+            'x1': self.points[1].x(),
+            'y1': self.points[1].y(),
+            'x2': self.points[2].x(),
+            'y2': self.points[2].y(),
+            'x3': self.points[3].x(),
+            'y3': self.points[3].y()
         }
 
     @staticmethod
@@ -30,6 +34,8 @@ class RectangleModel(AbstractAnotationShapeModel):
         """ Convert python dictionary information to RectangleModel object
         """
         points = []
+        points.append(QPoint(dict_['x0'], dict_['y0']))
         points.append(QPoint(dict_['x1'], dict_['y1']))
         points.append(QPoint(dict_['x2'], dict_['y2']))
+        points.append(QPoint(dict_['x3'], dict_['y3']))
         return RectangleModel(points=points)
