2252690a4eb90c05e66b87585bba7f9791688c73
mohammad javad <mohammadjavad.mirshekari@gmail.com>
2021-03-06 04:42:51 +0330

    Use 'fbs' for app context

--- labelImg.py
+++ labelImg.py
@@ -1473,31 +1473,7 @@ def get_main_app(argv=[]):
                      args.predefined_classes_file)
     win.show()
     return app.app, win
-
-def get_main_app(argv=[]):
-    """
-    Standard boilerplate Qt application code.
-    Do everything but app.exec_() -- so that we can test the application in one thread
-    """
-    # from fbs_runtime.application_context.PyQt5 import ApplicationContext
-    app = QApplication(argv)
-    app.setApplicationName(__appname__)
-    app.setWindowIcon(newIcon("app"))
-    # Tzutalin 201705+: Accept extra agruments to change predefined class file
-    argparser = argparse.ArgumentParser()
-    argparser.add_argument("image_dir", nargs="?")
-    argparser.add_argument("predefined_classes_file",
-                           default=os.path.join(os.path.dirname(
-                               __file__), "data", "predefined_classes.txt"),
-                           nargs="?")
-    # argparser.add_argument("save_dir", nargs="?")
-    args = argparser.parse_args(argv[1:])
-    # Usage : labelImg.py image predefClassFile saveDir
-    win = MainWindow(args.image_dir,
-                     args.predefined_classes_file)
-    win.show()
-    return app, win
-
+    
 def main():
     '''construct main app and run it'''
     app, _win = get_main_app(sys.argv)
