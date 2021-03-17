6f8fe60aff09b28262e42fc2616bdb1fd95179b8
mohammad javad <mohammadjavad.mirshekari@gmail.com>
2021-03-17 21:48:33 +0330

    Implement status bar for 'ImageIdPreviewListView'

--- libs/explorer/ImageIdPreviewListView.py
+++ libs/explorer/ImageIdPreviewListView.py
@@ -9,7 +9,7 @@ from ..mixins import AbstractExplorerViewMixin
 from ..imageViewItem import ImagePreviewItem
 
 
-class ImageIdPreviewListView(QListWidget, AbstractExplorerViewMixin):
+class ImageIdPreviewListView(QWidget, AbstractExplorerViewMixin):
     def __init__(
         self,
         parent=None,
@@ -20,10 +20,12 @@ class ImageIdPreviewListView(QListWidget, AbstractExplorerViewMixin):
         super().__init__()
         self.parent = parent
         self.itemWidgetComponent = itemWidgetComponent
-        self.itemClicked.connect(lambda item:
-                                 onClicked(self._translateIndex(item)))
-        self.itemDoubleClicked.connect(lambda item:
-                                       onDoubleClicked(self._translateIndex(item)))
+        
+        self.__listView = QListWidget(parent=self)
+        self.__listView.itemClicked.connect(lambda item: onClicked(self._translateIndex(item)))
+        self.__listView.itemDoubleClicked.connect(lambda item: onDoubleClicked(self._translateIndex(item)))
+        self.__status = QLabel()
+
         self._configStyle()
         self._items = []
 
@@ -32,13 +34,26 @@ class ImageIdPreviewListView(QListWidget, AbstractExplorerViewMixin):
         return self._items
 
     def _configStyle(self):
-        self.setFlow(QListView.LeftToRight)
-        self.setWrapping(True)
+        layout = QVBoxLayout()
+        layout.addWidget(self.__status)
+        layout.addWidget(self.__listView)
+        self.setLayout(layout)
+        
+        self.__listView.setFlow(QListView.LeftToRight)
+        self.__listView.setWrapping(True)
+
+    def item(self, indx):
+        """ return item with index 'indx'
+        """ 
+        return self.__listView.item(indx)
+
 
     def _translateIndex(self, index):
         item = index
-        iw = self.itemWidget(item)
+        iw = self.__listView.itemWidget(item)
         return iw
+
+
 
     def loadContent(self, imagesDataModel: List[ImageDataModel]):
         """ Load View content
@@ -47,14 +62,19 @@ class ImageIdPreviewListView(QListWidget, AbstractExplorerViewMixin):
             dModel.displayText = 'ID: {}'.format(dModel.extra['id']) 
         self._organizeLayout(imagesDataModel)
 
+    def setStatusText(self, text: str):
+        self.__status.setText(text)
+
     def _organizeLayout(self, imagesDataModel: List[ImageDataModel]):
-        self.clear()
+        self.__listView.clear()
+        self.__status.setText('')
         self._items = []
+
         for i, imageDataItem in enumerate(imagesDataModel):
             iw = self.itemWidgetComponent(imageDataItem, indx=i)
             self.items.append(iw)
             item = QListWidgetItem()
             item.setSizeHint(iw.sizeHint())
 
-            self.addItem(item)
-            self.setItemWidget(item, iw)
+            self.__listView.addItem(item)
+            self.__listView.setItemWidget(item, iw)
